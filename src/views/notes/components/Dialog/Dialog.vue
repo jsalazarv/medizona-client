2c6d97e519d84dbba373865a7d6bf3ee691d2ee3
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-07-14 18:22:31 -0500

    Modal style correction

--- src/views/notes/components/Dialog/Dialog.vue
+++ src/views/notes/components/Dialog/Dialog.vue
@@ -4,7 +4,6 @@
       <div class="modal-container">
         <div class="modal-body">
           <slot name="body"> </slot>
-          {{ data }}
         </div>
         <div class="modal-footer">
           <slot name="footer">
