8d5a9bb42e53b889e892f6bfc1aed21c30ad1cc2
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-07-14 10:14:48 -0500

    Added redirection to creation page

--- src/views/notes/CreateNotes.vue
+++ src/views/notes/CreateNotes.vue
@@ -1,16 +1,13 @@
 <template>
-  <div>
-    Crear notas
-    <Dialog />
-  </div>
+  <div>Crear notas</div>
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-property-decorator";
 import Component from "vue-class-component";
-import Dialog from "@/views/notes/components/Dialog/Dialog.vue";
+
 @Component({
-  components: { Dialog },
+  components: {},
 })
 export default class CreateNotes extends Vue {}
 </script>
