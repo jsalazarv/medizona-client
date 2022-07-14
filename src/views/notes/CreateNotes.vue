b53019c5d96a2fedc401a723280f6b39d2472d09
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-07-14 10:25:25 -0500

    Added base template for create view

--- src/views/notes/CreateNotes.vue
+++ src/views/notes/CreateNotes.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Crear notas</div>
+  <div class="flex justify-center">
+    <div class="container w-screen">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden">
+          <h1>Create note</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
